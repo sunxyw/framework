0d469c8e655c7dbe24dd9ee061a2469df1be8cb8
sunxyw <xy2496419818@gmail.com>
2019-10-19 20:43:42 +0800

    Add response function

--- src/functions.php
+++ src/functions.php
@@ -1,8 +1,14 @@
 <?php
 
 use Framework\Framework;
+use Framework\Response;
 
 function config($name, $default = false)
 {
     return Framework::getInstance()->getConfig($name, $default);
+}
+
+function response()
+{
+    return Response::getInstance();
 }
